83c3bce790cafdd768d03506fa7ec93cc8437562
Jona Subba <sanarung.subba@gmail.com>
2024-07-11 21:38:35 +0545

    changes

--- app/(app)/cart/page.jsx
+++ app/(app)/cart/page.jsx
@@ -1,23 +1,22 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import { useStore } from '@/app/zustand/store';
-import Link from 'next/link';
+import Image from "next/image";
+import { useStore } from "@/app/zustand/store";
+import Link from "next/link";
 import ItemCard from "@/app/_components/ItemCard";
 
 const Cart = () => {
-    const { products } = useStore((state) => ({
-        products: state.products,
-    }))
+  const { products } = useStore((state) => ({
+    products: state.products,
+  }));
 
+  return (
+    <div className="max-w-container mx-auto px-4 pt-10">
+      {products?.map((product) => (
+        <ItemCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
-    return (
-        <div className='max-w-container mx-auto px-4 pt-10'>
-            {products?.map((product) => (
-                <ItemCard key={product.id} product={product} />
-            ))}
-        </div>
-    )
-}
-
-export default Cart
+export default Cart;
